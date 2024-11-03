0a2c6c7030a8302383c20b85ebffa2e1ff1e2a26
Miles Liebren <miles.e.liebren@gmail.com>
2024-11-03 16:10:22 -0600

    Redid createGame.js to use MySQL

--- JavaScript/createGame.js
+++ JavaScript/createGame.js
@@ -1,106 +1,53 @@
-// create test data
+const mysql = require('mysql2');
+require('dotenv').config();
 
-// https://www.section.io/engineering-education/how-to-use-localstorage-with-javascript/
-    // look into storing objects in localStorage to better transition to DB
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
 
-// red-black student view
-localStorage.setItem("redBlackNum", 3);  // TODO: add record count instead of static number
-localStorage.setItem("redBlackHigh", 900); // TODO: add max instead of static number
-localStorage.setItem("redBlackLow", 400); // TODO: add min instead of static number
-localStorage.setItem("redBlackGPA", 633.33); // TODO: add calculation instead of static number
-// wheat-steel student view
-localStorage.setItem("wheatSteelNum", 2); // TODO: add record count instead of static number
-localStorage.setItem("wheatHigh", 100); // TODO: add max instead of static number
-localStorage.setItem("wheatGoalNum", 1 + "/" + 2 + " (" + 50 + "%)"); // TODO: add calculation instead of static numbers
-localStorage.setItem("steelHigh", 750); // TODO: add max instead of static number
-localStorage.setItem("steelGoalNum", 2 + "/" + 2 + " (" + 100 + "%)"); // TODO: add calculation instead of static numbers
+// Red-Black Game Statistics
+connection.query(
+    'SELECT COUNT(*) AS totalPlayers, MAX(score_total) AS highScore, MIN(score_total) AS lowScore, AVG(score_total) AS avgScore FROM rb_player_score;',
+    (err, results) => {
+      if (err) throw err;
+      const { totalPlayers, highScore, lowScore, avgScore } = results[0];
+      
+      console.log(`Total Players: ${totalPlayers}`);
+      console.log(`Highest Score: ${highScore}`);
+      console.log(`Lowest Score: ${lowScore}`);
+      console.log(`Average Score: ${avgScore}`);
+    }
+  );
+  
 
-/* red-black game view */
-// game info
-localStorage.setItem("gameName_1", "Red Card Black Card");
-localStorage.setItem("gameDate_1", "10/13/23");
-localStorage.setItem("playerNum_1", 24); 
-localStorage.setItem("pairNum_1", 12);
-localStorage.setItem("roundNum_1", 8);
-// game stats 
-localStorage.setItem("blackTotal", 88); 
-localStorage.setItem("blackAverage", 11);
-localStorage.setItem("redTotal", 104); 
-localStorage.setItem("redAverage", 13);
-// player stats (general)
-localStorage.setItem("highestName", "Jack");
-localStorage.setItem("highestScore", 1400);
-localStorage.setItem("lowestName", "Sam");
-localStorage.setItem("lowestScore", 200);
-// player stats (specific)
-localStorage.setItem("highestBlackName", "Sam");
-localStorage.setItem("highestBlackScore", 7);
-localStorage.setItem("lowestBlackName", "Jack");
-localStorage.setItem("lowestBlackScore", 1);
-localStorage.setItem("highestRedName", "Jack");
-localStorage.setItem("highestRedScore", 7);
-localStorage.setItem("lowestRedName", "Sam");
-localStorage.setItem("lowestRedScore", 1);
+  // Wheat-Steel Game Statistics
+connection.query(
+    `SELECT SUM(wheat_produced) AS wheatTotal, AVG(wheat_produced) AS wheatAverage,
+            SUM(steel_produced) AS steelTotal, AVG(steel_produced) AS steelAverage
+     FROM round_score WHERE game_id = 2;`,
+    (err, results) => {
+      if (err) throw err;
+      const { wheatTotal, wheatAverage, steelTotal, steelAverage } = results[0];
+  
+      console.log(`Total Wheat Produced: ${wheatTotal}`);
+      console.log(`Average Wheat Produced: ${wheatAverage}`);
+      console.log(`Total Steel Produced: ${steelTotal}`);
+      console.log(`Average Steel Produced: ${steelAverage}`);
+    }
+  );
 
-/* wheat-steel game view */
-// game info
-localStorage.setItem("gameName_2", "Wheat & Steel");
-localStorage.setItem("gameDate_2", "10/16/23");
-localStorage.setItem("playerNum_2", 24); 
-localStorage.setItem("teamsFour", 6);
-localStorage.setItem("periodNum", 5);
-// game results
-localStorage.setItem("bothGoals", ["The Dealers", "Jad"]); 
-localStorage.setItem("oneGoal", ["Economics", "The Traders", "Steal", "Wheat"]); 
-localStorage.setItem("noGoals", ["Micro", "Wall Street"]); 
-// wheat stats
-localStorage.setItem("wheatProduceTotal", 1849);
-localStorage.setItem("wheatProduceAverage", 369.8);
-localStorage.setItem("wheatConsumeTotal", 1849);
-localStorage.setItem("wheatConsumeAverage", 369.8);
-localStorage.setItem("wheatTradeTotal", 613);
-localStorage.setItem("wheatTradeAverage", 122.6);
-// steel stats
-localStorage.setItem("steelProduceTotal", 1849);
-localStorage.setItem("steelProduceAverage", 369.8);
-localStorage.setItem("steelConsumeTotal", 1849);
-localStorage.setItem("steelConsumeAverage", 369.8);
-localStorage.setItem("steelTradeTotal", 613);
-localStorage.setItem("steelTradeAverage", 122.6);
-
-// game info
-localStorage.setItem("roundOneWheat", 0);
-localStorage.setItem("roundTwoWheat", 0);
-localStorage.setItem("roundThreeWheat", 0);
-localStorage.setItem("roundFourWheat", 0);
-localStorage.setItem("roundFiveWheat", 0);
-
-localStorage.setItem("roundOneSteel", 150);
-localStorage.setItem("roundTwoSteel", 150);
-localStorage.setItem("roundThreeSteel", 150);
-localStorage.setItem("roundFourSteel", 150);
-localStorage.setItem("roundFiveSteel", 150);
-
-localStorage.setItem("roundOneWheatTrade", "-");
-localStorage.setItem("roundTwoWheatTrade", 0);
-localStorage.setItem("roundThreeWheatTrade", 0);
-localStorage.setItem("roundFourWheatTrade", "+100");
-localStorage.setItem("roundFiveWheatTrade", "+100");
-
-localStorage.setItem("roundOneSteelTrade", "-");
-localStorage.setItem("roundTwoSteelTrade", 0);
-localStorage.setItem("roundThreeSteelTrade", 0);
-localStorage.setItem("roundFourSteelTrade", "-150");
-localStorage.setItem("roundFiveSteelTrade", "-150");
-
-localStorage.setItem("roundOneWheatConsume", 0);
-localStorage.setItem("roundTwoWheatConsume", 0);
-localStorage.setItem("roundThreeWheatConsume", 0);
-localStorage.setItem("roundFourWheatConsume", 100);
-localStorage.setItem("roundFiveWheatConsume", 100);
-
-localStorage.setItem("roundOneSteelConsume", 150);
-localStorage.setItem("roundTwoSteelConsume", 150);
-localStorage.setItem("roundThreeSteelConsume", 150);
-localStorage.setItem("roundFourSteelConsume", 0);
-localStorage.setItem("roundFiveSteelConsume", 0);
+  // Fetch game title and date
+connection.query(
+    'SELECT game_title, create_dt FROM game_catalog JOIN game_session ON game_catalog.game_id = game_session.game_id;',
+    (err, results) => {
+      if (err) throw err;
+      results.forEach(game => {
+        console.log(`Game Name: ${game.game_title}, Date: ${game.create_dt}`);
+      });
+    }
+  );
+  
+  connection.end();
