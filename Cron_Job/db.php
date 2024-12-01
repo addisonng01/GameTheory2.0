8003d134c3f3a17c874a1bd623a093fc59849e0e
Joe <jtschida2@css.edu>
2024-11-30 18:06:24 -0600

    Updated db.php files and routing for second index file - Joe Tschida

--- Cron_Job/db.php
+++ Cron_Job/db.php
@@ -1,11 +1,13 @@
 <?php
 
-$host = "localhost";
-$dbname = "txlxmqol_Game_Theory_DB";
-$username = "txlxmqol_webToDB";
-$password = "ItsBeginingToLookLikeChristmas";
+include('../../../env.php');
 
-$mysqli = new mysqli($host, $username, $password, $dbname);
+$dbHost = $_ENV['DB_HOST'] ?? 'default_host';
+$dbName = $_ENV['DB_NAME'] ?? 'default_name';
+$dbUser = $_ENV['DB_USER'] ?? 'default_user';
+$dbPass = $_ENV['DB_PASS'] ?? 'default_password';
+
+$mysqli = new mysqli($dbhost, $dbUser, $dbPass, $dbname);
 
 if ($mysqli->connect_errno) {
     die("Connection error: ". $mysqli->connect_error);
--- HTML/teacherViews/TeacherRedBlackPHP/db.php
+++ HTML/teacherViews/TeacherRedBlackPHP/db.php
@@ -7,7 +7,7 @@ $dbName = $_ENV['DB_NAME'] ?? 'default_name';
 $dbUser = $_ENV['DB_USER'] ?? 'default_user';
 $dbPass = $_ENV['DB_PASS'] ?? 'default_password';
 
-$mysqli = new mysqli($host, $username, $password, $dbname);
+$mysqli = new mysqli($dbhost, $dbUser, $dbPass, $dbname);
 
 if ($mysqli->connect_errno) {
     die("Connection error: ". $mysqli->connect_error);
