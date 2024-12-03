c6fde9e8323c074847eb584cc4dcd7fb3b955d8f
Joe <jtschida2@css.edu>
2024-12-02 23:25:04 -0600

    Fix another error in Db.php

--- HTML/teacherViews/TeacherRedBlackPHP/db.php
+++ HTML/teacherViews/TeacherRedBlackPHP/db.php
@@ -7,7 +7,7 @@ $dbName = $_ENV['DB_NAME'] ?? 'default_name';
 $dbUser = $_ENV['DB_USER'] ?? 'default_user';
 $dbPass = $_ENV['DB_PASS'] ?? 'default_password';
 
-$mysqli = new mysqli($dbHost, $dbUser, $dbHost, $dbName);
+$mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
 
 if ($mysqli->connect_errno) {
     die("Connection error: ". $mysqli->connect_error);
